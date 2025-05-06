258133c1c1a326ac96e3d39ec4c9d8f6838af103
Void <80608102+xptea@users.noreply.github.com>
2025-05-05 20:05:52 -0400

    eletron react ts tailwind

---
+++ src/renderer/App.tsx
@@ -0,0 +1,60 @@
+import { MemoryRouter as Router, Switch, Route } from 'react-router-dom';
+import 'tailwindcss/tailwind.css';
+import icon from '../../assets/icon.svg';
+import './App.css';
+
+const Tailwind = () => {
+  return (
+    <div className="text-center border-solid border-4 border-red-500 bg-black text-white shadow">
+      ERB + TAILWIND = ❤
+    </div>
+  );
+};
+
+const Hello = () => {
+  return (
+    <div>
+      <div className="Hello">
+        <img width="200px" alt="icon" src={icon} />
+      </div>
+      <h1>electron-react-boilerplate</h1>
+      <Tailwind />
+      <div className="Hello">
+        <a
+          href="https://electron-react-boilerplate.js.org/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button type="button">
+            <span role="img" aria-label="books">
+              📚
+            </span>
+            Read our docs
+          </button>
+        </a>
+        <a
+          href="https://github.com/sponsors/electron-react-boilerplate"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button type="button">
+            <span role="img" aria-label="books">
+              🙏
+            </span>
+            Donate
+          </button>
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" component={Hello} />
+      </Switch>
+    </Router>
+  );
+}
